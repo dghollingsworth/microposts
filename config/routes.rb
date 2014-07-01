87b3835d4e5c64e51707f5262d21e5e9318ccc93
dghollingsworth <dghollingsworth@gmail.com>
2014-06-30 21:38:24 -0400

    Add insultingly basic contact page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Microposts::Application.routes.draw do
   get "static_pages/home", as: "home"
   get "static_pages/help", as: "help"
 	get "static_pages/about", as: "about"  
+	get "static_pages/contact", as: "contact"
   
 end
