bb524f35936019d23bfd68aba2a6dda0718166f5
dghollingsworth <dghollingsworth@gmail.com>
2014-07-17 18:33:27 -0400

    wrote new tests and refactored old tests for static_pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Microposts::Application.routes.draw do
   root "static_pages#home"
-  get "users/new"
 
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
